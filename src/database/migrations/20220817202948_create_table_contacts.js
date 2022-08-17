53b94a160ad5bfa26e37bad6ed3a14bd252279d5
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-17 17:49:10 -0300

    create migrate table contacts

---
+++ src/database/migrations/20220817202948_create_table_contacts.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) =>
+  knex.schema.createTable("contacts", (table) => {
+    table.increments("id");
+    table.integer("user_id").notNullable();
+    table.text("name").notNullable();
+    table.text("email").notNullable();
+    table.text("phone").notNullable();
+    table
+      .foreign("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+  });
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.dropTable("contacts");

f21ca63dbd4d4fbab3b5f90540caa34409a23b0f
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-17 13:14:44 -0300

    altered index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require("./server");
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log(`Sever running at http://localhost:${process.env.PORT}`)
+  console.log(`🔥 Server running at http://localhost:${process.env.PORT}`)
 );

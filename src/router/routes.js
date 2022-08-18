e5bc8e589c197a45e1cfc80d945f469d1fb208ee
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-17 21:24:32 -0300

    add contact routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,20 @@
 const { Router } = require("express");
 const login = require("../controllers/login");
 const register = require("../controllers/user");
+const verifyLogin = require("../middleware/verifyLogin");
+const contactsController = require("../controllers/contacts");
 
 const routes = Router();
 
 routes.post("/login", login);
 routes.post("/usuarios", register);
+
+routes.use(verifyLogin);
+
+routes.get("/contatos", contactsController.getAllContacts);
+routes.get("/contatos/:id", contactsController.getContact);
+routes.post("/contatos", contactsController.createContact);
+routes.delete("/contatos/:id", contactsController.deleteContact);
+routes.put("/contatos/:id", contactsController.editContact);
 
 module.exports = routes;

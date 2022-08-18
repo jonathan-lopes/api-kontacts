554ed2b779ba16674287f58f8b56cb4e2feab19b
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-17 21:22:36 -0300

    add schema for contacts

---
+++ src/validations/contactsSchema.js
@@ -0,0 +1,15 @@
+const yup = require("./yupSettings");
+
+const contactsSchema = yup.object().shape({
+  name: yup.string().trim().required(),
+  email: yup.string().email("Formato de e-mail inválido").required(),
+  phone: yup
+    .string()
+    .strict()
+    .matches(
+      /^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/,
+      "Fomato de telefone inválido"
+    ),
+});
+
+module.exports = contactsSchema;

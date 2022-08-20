d104ee68cbe2718ed8e05e39803f5fe7e94065bb
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-20 01:06:26 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const router = require("./router/routes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json());
 app.use(router);
 

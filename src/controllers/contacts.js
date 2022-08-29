76bd48812acfbf6a2c9217b3a50cc935fa4546c6
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-29 18:54:08 -0300

    removed try catch from contacts context

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,84 +1,61 @@
 const knex = require("../database");
+const { CrudError } = require("../helpers/apiErrors");
 const contactsSchema = require("../validations/contactsSchema");
 
 const getAllContacts = async (req, res) => {
   const { id } = req.user;
 
-  try {
-    const contacts = await knex("contacts").where("user_id", id);
-    return res.json(contacts);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
+  const contacts = await knex("contacts").where("user_id", id);
+  return res.json(contacts);
 };
 
 const getContact = async (req, res) => {
   const { id: userID } = req.user;
   const { id } = req.params;
 
-  try {
-    const contact = await knex("contacts")
-      .where({ user_id: userID, id })
-      .first();
+  const contact = await knex("contacts").where({ user_id: userID, id }).first();
 
-    return res.json(contact);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
+  return res.json(contact);
 };
 
 const createContact = async (req, res) => {
   const { name, email, phone } = req.body;
   const { id } = req.user;
 
-  try {
-    await contactsSchema.validate({ name, email, phone });
+  await contactsSchema.validate({ name, email, phone });
 
-    const insertContact = await knex("contacts").insert({
-      user_id: id,
-      name,
-      email,
-      phone,
-    });
+  const insertContact = await knex("contacts").insert({
+    user_id: id,
+    name,
+    email,
+    phone,
+  });
 
-    if (!insertContact) {
-      return res
-        .status(500)
-        .json({ message: "Não foi possível cadastrar o contato" });
-    }
-
-    return res.status(201).json();
-  } catch (error) {
-    return res
-      .status(error.name === "ValidationError" ? 400 : 500)
-      .json({ message: error.message });
+  if (!insertContact) {
+    throw new CrudError("Não foi possível cadastrar o contato");
   }
+
+  return res.status(201).json();
 };
 
 const deleteContact = async (req, res) => {
   const { id: userID } = req.user;
   const { id } = req.params;
 
-  try {
-    const contact = await knex("contacts").where({ id }).first();
+  const contact = await knex("contacts").where({ id }).first();
 
-    if (contact.user_id === Number(userID)) {
-      const deleteContact = await knex("contacts")
-        .del()
-        .where({ user_id: userID, id });
+  if (contact.user_id === Number(userID)) {
+    const deleteContact = await knex("contacts")
+      .del()
+      .where({ user_id: userID, id });
 
-      if (!deleteContact) {
-        return res
-          .status(500)
-          .json({ message: "Não foi possível deletar o contato" });
-      }
-
-      return res.status(204).json();
-    } else {
-      return res.status(401).json({ message: "Não autorizado" });
+    if (!deleteContact) {
+      throw new CrudError("Não foi possível deletar o contato");
     }
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
+
+    return res.status(204).json();
+  } else {
+    return res.status(401).json({ message: "Não autorizado" });
   }
 };
 
@@ -87,35 +64,27 @@ const editContact = async (req, res) => {
   const { id: userID } = req.user;
   const { id } = req.params;
 
-  try {
-    await contactsSchema.validate({ name, email, phone });
+  await contactsSchema.validate({ name, email, phone });
 
-    const contact = await knex("contacts").where({ id }).first();
+  const contact = await knex("contacts").where({ id }).first();
 
-    if (contact.user_id === Number(userID)) {
-      const updateContact = await knex("contacts")
-        .update({
-          user_id: userID,
-          name,
-          email,
-          phone,
-        })
-        .where({ id });
+  if (contact.user_id === Number(userID)) {
+    const updateContact = await knex("contacts")
+      .update({
+        user_id: userID,
+        name,
+        email,
+        phone,
+      })
+      .where({ id });
 
-      if (!updateContact) {
-        return res
-          .status(500)
-          .json({ message: "Não foi possível atualizar o contato" });
-      }
-
-      return res.status(204).json();
-    } else {
-      return res.status(401).json({ message: "Não autorizado" });
+    if (!updateContact) {
+      throw new CrudError("Não foi possível atualizar o contato");
     }
-  } catch (error) {
-    return res
-      .status(error.name === "ValidationError" ? 400 : 500)
-      .json({ message: error.message });
+
+    return res.status(204).json();
+  } else {
+    return res.status(401).json({ message: "Não autorizado" });
   }
 };
 

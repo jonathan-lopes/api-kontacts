113bd3fb00a7fe2147b32ec3dd025fbd42a145a9
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-29 18:54:40 -0300

    removed try catch from user context

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,39 +1,32 @@
 const knex = require("../database");
 const userSchema = require("../validations/userSchema");
 const bcrypt = require("bcrypt");
+const { ForbiddenError, CrudError } = require("../helpers/apiErrors");
 
 const register = async (req, res) => {
   const { name, email, passwd } = req.body;
 
-  try {
-    await userSchema.validate({ name, email, passwd });
+  await userSchema.validate({ name, email, passwd });
 
-    const userEmail = await knex("users").where({ email }).first();
+  const userEmail = await knex("users").where({ email }).first();
 
-    if (userEmail) {
-      return res.status(403).json({ message: "E-mail já cadastrado" });
-    }
-
-    const pwdCrypt = await bcrypt.hash(passwd, Number(process.env.SALT_ROUNDS));
-
-    const insertUser = await knex("users").insert({
-      name,
-      email,
-      passwd: pwdCrypt,
-    });
-
-    if (insertUser === 0) {
-      return res
-        .status(500)
-        .json({ message: "Não foi possível cadastrar o usuário" });
-    }
-
-    return res.status(201).json();
-  } catch (error) {
-    return res
-      .status(error.name === "ValidationError" ? 400 : 500)
-      .json({ message: error.message });
+  if (userEmail) {
+    throw new ForbiddenError("E-mail já cadastrado");
   }
+
+  const pwdCrypt = await bcrypt.hash(passwd, Number(process.env.SALT_ROUNDS));
+
+  const insertUser = await knex("users").insert({
+    name,
+    email,
+    passwd: pwdCrypt,
+  });
+
+  if (!insertUser) {
+    throw new CrudError("Não foi possível cadastrar o usuário");
+  }
+
+  return res.status(201).json();
 };
 
 module.exports = register;

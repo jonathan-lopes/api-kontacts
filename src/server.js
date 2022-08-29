4c6224386bb1823658ec637c54359997497a79d6
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-29 18:46:20 -0300

    add error middleware

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const router = require("./router/routes");
 const cors = require("cors");
+const ErrorMiddleware = require("./middlewares/error");
 
 const app = express();
 
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json());
 app.use(router);
+app.use(ErrorMiddleware);
 
 module.exports = app;

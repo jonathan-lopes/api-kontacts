9f3f338173a76f3f20c4c2b285da413a7ac3abae
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-29 18:50:06 -0300

    add error classes

---
+++ src/helpers/apiErrors.js
@@ -0,0 +1,45 @@
+class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+class BadRequestError extends ApiError {
+  constructor(message) {
+    super(message, 400);
+  }
+}
+
+class NotFoundError extends ApiError {
+  constructor(message) {
+    super(message, 404);
+  }
+}
+
+class UnauthorizedError extends ApiError {
+  constructor(message) {
+    super(message, 401);
+  }
+}
+
+class ForbiddenError extends ApiError {
+  constructor(message) {
+    super(message, 403);
+  }
+}
+
+class CrudError extends ApiError {
+  constructor(message) {
+    super(message, 500);
+  }
+}
+
+module.exports = {
+  ApiError,
+  BadRequestError,
+  ForbiddenError,
+  NotFoundError,
+  UnauthorizedError,
+  CrudError,
+};
